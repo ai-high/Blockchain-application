0dd99e91c5faf15387d8f4e7c620f615cc988617
Zac Moran <zac.moran@icloud.com>
2019-01-11 07:53:34 +1100

    Peer to peer server up and working, decentralisation achieved!!!

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyPaser.json());
 
@@ -17,8 +19,11 @@ app.post('/mine', (req,res) => {
 	const block = bc.addBlock(req.body.data);
 	console.log(`New Block added: ${block.toString()}`);
 
+	p2pServer.syncChains();
+
 	res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
 
